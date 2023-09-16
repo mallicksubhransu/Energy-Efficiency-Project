1a2fdda46aaaacc076a0d9437d177d7b25ac92d1
Subhransu Sekhar Mallick <114018899+mallicksubhransu@users.noreply.github.com>
2023-09-16 21:39:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import pickle
 import streamlit as st
 
 # Load the saved models
-loaded_model1 = pickle.load(open("Heating load_model.sav", 'rb'))
-loaded_model2 = pickle.load(open("Cooling load_model.sav", 'rb'))
+loaded_model1 = pickle.load(open("D:\Datascience\Data Science Project Internships\iNeuron\Energy Efficiency Project\ENB SSM\Heating load_model.sav", 'rb'))
+loaded_model2 = pickle.load(open("D:\Datascience\Data Science Project Internships\iNeuron\Energy Efficiency Project\ENB SSM\Cooling load_model.sav", 'rb'))
 
 def building_load_prediction(input_data):
     # Convert input_data to float and reshape
@@ -19,15 +19,7 @@ def main():
     st.title('Energy Efficient Building Load Calculation Web App')
     
     # Collect input data from the user
-    #RelativeCompactness = st.text_input('Relative compactness value')
-    #SurfaceArea = st.text_input('Surface Area value')
-    #WallArea = st.text_input('Wall Area value')
-    #RoofArea = st.text_input('Roof Area value')
-    #OverallHeight = st.text_input('Overall Height value')
-    #Orientation = st.text_input('Orientation value')
-    #GlazingArea = st.text_input('Glazing Area value')
-    #GlazingAreaDistribution = st.text_input('Glazing Area Distribution value')
-
+    
     # getting the input data from the user
     col1, col2 = st.columns(2)
     
@@ -59,18 +51,13 @@ def main():
     HeatingLoad_result = ""
     CoolingLoad_result = ""
     
-    input_data = [RelativeCompactness, SurfaceArea, WallArea, RoofArea, OverallHeight, Orientation, GlazingArea, GlazingAreaDistribution]
-    
     # Check if the user has clicked the prediction button
-    if st.button('Calculate Heating Load'):
-        # input_data = [RelativeCompactness, SurfaceArea, WallArea, RoofArea, OverallHeight, Orientation, GlazingArea, GlazingAreaDistribution]
-        heating_load = building_load_prediction(input_data)
+    if st.button('Estimate Heating Load'):
+        input_data = [RelativeCompactness, SurfaceArea, WallArea, RoofArea, OverallHeight, Orientation, GlazingArea, GlazingAreaDistribution]
+        heating_load, cooling_load = building_load_prediction(input_data)
         
-        HeatingLoad_result = f"Calculated Heating Load value is: {heating_load[0]}"  # Display heating load
-        # CoolingLoad_result = f"Calculated Cooling Load value is: {cooling_load[0]}"  # Display cooling load
-    if st.button('Calculate Cooling Load'):
-        cooling_load = building_load_prediction(input_data)
-        CoolingLoad_result = f"Calculated Cooling Load value is: {cooling_load[0]}"  # Display cooling load
+        HeatingLoad_result = f"Estimated Heating Load value is: {heating_load[0]}"  # Display heating load
+        CoolingLoad_result = f"Estimated Cooling Load value is: {cooling_load[0]}"  # Display cooling load
     
     # Display the results in the Streamlit app
     st.success(HeatingLoad_result)
